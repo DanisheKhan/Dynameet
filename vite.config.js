6fc8bb0c8275ad25ca2c65d20373fa8deca75490
DanisheKhan <khandanishe125@gmail.com>
2025-12-31 02:17:55 +0530

    refactor: remove image optimization plugin from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
-import { ViteImageOptimizer } from '@vheemstra/vite-plugin-imagemin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    ViteImageOptimizer({
-      // Optimize PNG images
-      png: {
-        quality: 80,
-      },
-      // Optimize JPEG images  
-      jpeg: {
-        quality: 80,
-      },
-      // Optimize JPG images
-      jpg: {
-        quality: 80,
-      },
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   base: '/',
   build: {
     rollupOptions: {
